66a0044b2e9d0e6123f2d2665e80305f0a14c1bf
Sabeena <sabeenarayyan13@gmail.com>
2021-07-18 07:15:37 +0400

    Add project files.

---
+++ studioclass1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace studioclass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            Console.WriteLine("Enter the radius");
+            string radius = Console.ReadLine();
+            int r = int.Parse(radius); // MPG
+
+
+            double areaOfCircle = pi * r * r;
+            Console.WriteLine("The area of the circle is " + areaOfCircle);
+
+            double circumference = 2 * pi * r;
+            double diameter = 2 * r; // MILES
+            double gallonsPerRound = diameter / r;
+            Console.WriteLine("Gallons of gas they will use to go around this circle is " + gallonsPerRound);
+            Console.WriteLine("The circumference is " + circumference);
+            Console.WriteLine("The diameter is " + diameter);
+        }
+    }
+}
